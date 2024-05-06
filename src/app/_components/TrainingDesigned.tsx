e562f396696b47cee69c9bfd4b8cc065f3782c15
lazaro <lazaroalves12355@gmail.com>
2024-05-06 18:53:13 -0300

    v2.2

--- src/app/_components/TrainingDesigned.tsx
+++ src/app/_components/TrainingDesigned.tsx
@@ -12,7 +12,7 @@ export const TrainingDesigned = () => {
                         <Trainingh1>Projetadas para você</Trainingh1>
                     </TrainingDivTitle>
                     <TrainingDivContainerImg>
-                        <CardTraining href="/img/Personal_Trainner.jpg" alt="Pessoa fazendo exerício" name="Personal Training" />
+                        <CardTraining href="/img/Treino_Personal_Trainner.jpg" alt="Pessoa fazendo exerício" name="Personal Training" />
                         <CardTraining href="/img/Treino_CordaNaval.jpg" alt="Mulher fazendo exercicio corda naval" name="Sessão  com corda naval " />
                         <CardTraining href="/img/Treino_Esteira.jpg" alt="Mulher fazendo exercicio de corrida na esteira" name="Sessão  de Corrida" />
                         <CardTraining href="/img/Treino_Forca.jpg" alt="Mulher fazendo exercicio de levantamento de peso" name="Sessão  de Força" />
